1e1a143199529491f73a8a08c19f181244f13eb5
mabelsueh <su.eh.mabel@gmail.com>
2020-12-04 14:07:12 +0000

    trying validation

--- validation.js
+++ validation.js
@@ -1,8 +1,12 @@
-document.querySelector('#form').addEventListener('submit', function(){
+document.querySelector('#form').addEventListener('submit', function(event){
+event.preventDefault();
+
+    // error flags
     let nameIsInvalid = false;
     let emailIsInvalid = false;
-    let messageIsInvalid= false;
+    let messageIsInvalid = false;
 
+    // conditions
     let name = document.querySelector('#name').value;
     if (name.length < 3){
         nameIsInvalid= true;
@@ -18,18 +22,20 @@ document.querySelector('#form').addEventListener('submit', function(){
         messageIsInvalid= true;
     }
 
+    // showing error
     if (nameIsInvalid) {
-    document.querySelector('#nameIsInvalid').innerHTML = "*Name should be at least 2 characters.";
+    document.querySelector('#nameIsInvalid').innerHTML = "*Name should be at least 2 characters";
     }
     
     if (emailIsInvalid){
-     document.querySelector('#emailInvalid').innerHTML="*Email is invalid";
+     document.querySelector('#emailIsInvalid').innerHTML="*Email should contain '@' and '.'";
     }
 
     if (messageIsInvalid) {
-    document.querySelector('#messageIsInvalid').innerHTML = "*Message is too short.";
+    document.querySelector('#messageIsInvalid').innerHTML = "*Message is too short";
     }
 
+    // submit
     if (!nameIsInvalid && !emailIsInvalid && !messageIsInvalid) {
     alert("Thank you for reaching out to us. We will get back to you as soon as possible!");
     document.querySelector('form').submit();
